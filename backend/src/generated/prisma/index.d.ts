f716e0f615eac12c523d9f0c31fba118a8e82027
lenonmerlo <lenontm@gmail.com>
2025-06-06 09:40:39 -0300

    feat: CRUD completo de treinador com validações, hash de senha e perfil 'professor'

--- backend/src/generated/prisma/index.d.ts
+++ backend/src/generated/prisma/index.d.ts
@@ -48,6 +48,7 @@ export class PrismaClient<
   U = 'log' extends keyof ClientOptions ? ClientOptions['log'] extends Array<Prisma.LogLevel | Prisma.LogDefinition> ? Prisma.GetEvents<ClientOptions['log']> : never : never,
   ExtArgs extends $Extensions.InternalArgs = $Extensions.DefaultArgs
 > {
+  treinador: any;
   [K: symbol]: { types: Prisma.TypeMap<ExtArgs>['other'] }
 
     /**
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import equipeRoutes from './equipeRoutes';
 import atletaRoutes from './atletaRoutes';
 import authRoutes from './authRoutes';
+import treinadorRoutes from './treinadorRoutes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/equipes', equipeRoutes);
 router.use('/atletas', atletaRoutes);
+router.use('/treinadores', treinadorRoutes);
 
 export default router;
